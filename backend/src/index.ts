db58a3c3697cac238d498328a55aa91c295e03b0
juan jose ospina <juan.ospina14@utp.edu.co>
2026-05-20 19:17:43 -0500

    Cambiar título a Gestión de Proyectos

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -12,7 +12,12 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(cors());
+app.use(cors(
+  origin: ['http://localhost:4200', 'https://main.d2aa7p7p1xeg8i.amplifyapp.com'],
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.set('etag', false);
 app.use((req, res, next) => {
